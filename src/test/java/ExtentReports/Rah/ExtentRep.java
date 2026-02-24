6f450704a767e0a616eba8d0c0710be0144a1ec9
R-SProjects <gadgets785@gmail.com>
2026-02-24 19:45:58 +0530

    Initial commit for Rah project

---
+++ src/test/java/ExtentReports/Rah/ExtentRep.java
@@ -0,0 +1,36 @@
+package ExtentReports.Rah;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentRep {
+	ExtentReports extent;
+	@BeforeTest
+	public void configdemo() {
+		//ExtendReports,ExtentSparkReporter
+		String path=System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter=new ExtentSparkReporter(path);
+		reporter.config().setReportName("WebAuto");
+		reporter.config().setDocumentTitle("New TiTle");
+		extent=new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Roopa");
+	}
+	
+	@Test
+	public void initialDemo() {
+		ExtentTest test =extent.createTest("Initial Demo");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		System.out.println(driver.getTitle());
+		test.fail("Results not match");
+		extent.flush();
+	}
+
+}
